8d52eb38b756d0008a6d1cd715c825b1cacfe542
fine-proj <fine-proj@no-spam.ws>
2018-03-17 18:55:07 +0200

    display main menu

--- app/Http/Controllers/SiteController.php
+++ app/Http/Controllers/SiteController.php
@@ -4,6 +4,7 @@ namespace Corp\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Corp\Repositories\MenusRepository;
+use Lavary\Menu\Facade as Menu;
 
 class SiteController extends Controller{
 
@@ -26,9 +27,9 @@ class SiteController extends Controller{
 
     public function renderOutput(){
         $menu = $this->getMenu();
-        dd($menu);
+        //dd($menu);
 
-        $navigation = view(env('THEME') . '.navigation')->render();
+        $navigation = view(env('THEME') . '.navigation')->with('menu',$menu)->render();
         $this->vars['navigation'] = $navigation;
         return view($this->template)->with($this->vars);
     }
@@ -36,6 +37,22 @@ class SiteController extends Controller{
     public function getMenu()
     {
         $menu = $this->m_rep->get();
+        $mBuilder = Menu::make('MyNav', function($m)use($menu){
+            foreach($menu as $item)
+            {
+                if($item->parent == 0)
+                {	$m->add( $item->title, $item->path )->id($item->id);
+                }
+                else
+                {
+                    if($m->find( $item->parent ))
+                    {
+                        $m->find( $item->parent )->add( $item->title, $item->path )->id($item->id);
+                    }
+                }
+            }
+        });
+        //dd($mBuilder);
         return $menu;
     }
 }
